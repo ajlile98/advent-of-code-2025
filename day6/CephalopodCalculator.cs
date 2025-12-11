c7872b58b6855e85da157724c4ec64fc2a52c902
Andy <ajlile98@gmail.com>
2025-12-10 23:27:50 -0500

    day6 star1 solution!

--- day6/CephalopodCalculator.cs
+++ day6/CephalopodCalculator.cs
@@ -1,19 +1,49 @@
 using System;
+using System.Linq.Expressions;
+using Microsoft.VisualBasic;
 
 namespace day6;
 
-public class CephalopodCalculator()
+public class CephalopodCalculator(char[] operators, string[] lines)
 {
-  public int ProcessInput(string input)
+  char[] Operators = operators;
+  long[] RunningTotals = new long[lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Length];
+  string[] InputLines = lines;
+  public long CalculateTotal()
   {
-    var lines = input.Split();
-    foreach(var line in lines)
+    for (int i = 0; i < InputLines.Length; i++)
     {
-      foreach(var value in line.Split())
+      var operands = InputLines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+      Console.WriteLine($"operands: {string.Join(',', operands)}");
+      for(int j = 0; j < operands.Length; j++)
       {
-        if(value)
+        var operand_str = operands[j];
+        if (string.IsNullOrWhiteSpace(operand_str)) continue;
+        long operand = long.Parse(operand_str);
+        if (RunningTotals[j] == 0)
+          RunningTotals[j] = operand;
+        else
+        {
+          RunningTotals[j] = Operate(Operators[j], RunningTotals[j], operand);
+        }
+          Console.WriteLine($"RunningTotals: {string.Join(',', RunningTotals)}");
       }
     }
-    return 0;
+    Console.WriteLine($"Final RunningTotals: {string.Join(',', RunningTotals)}");
+    return RunningTotals.Sum();
+  }
+
+  private long Operate(char op, long x, long y)
+  {
+    switch (op)
+    {
+      case '+':
+        return x + y;
+      case '*':
+        return x * y;
+      default:
+        throw new Exception($"Operator {op} not supported");
+    }
+
   }
 }
--- day6/Program.cs
+++ day6/Program.cs
@@ -1,7 +1,18 @@
 ﻿
+using day6;
+
 var input = """
 123 328  51 64 
  45 64  387 23 
   6 98  215 314
 *   +   *   +  
 """;
+
+// var lines = input.Split('\n');
+var lines = File.ReadLines("input.dat").ToArray();
+var Operators = lines[^1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => s[0]).ToArray();
+
+var calc = new CephalopodCalculator(operators: Operators, lines: lines[0..^1]);
+var result = calc.CalculateTotal();
+Console.WriteLine($"Total: {result}");
+
