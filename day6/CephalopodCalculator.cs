26b4d8b9b52c148aa021e5529c0254c10d4a7778
Andrew Lile <ajlile98@gmail.com>
2025-12-10 22:24:14 -0500

    day6 init

---
+++ day6/CephalopodCalculator.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace day6;
+
+public class CephalopodCalculator()
+{
+  public int ProcessInput(string input)
+  {
+    var lines = input.Split();
+    foreach(var line in lines)
+    {
+      foreach(var value in line.Split())
+      {
+        if(value)
+      }
+    }
+    return 0;
+  }
+}
